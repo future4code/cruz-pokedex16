d741b21f4c0f8381bcfdbd795c739b1d255c0c62
TheusH7X5 <matheusa7x.23@gmail.com>
2021-04-20 14:57:23 -0300

    header finalizado

--- pokedex/src/pages/Pokedex.js
+++ pokedex/src/pages/Pokedex.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './Header'
 import { Box, Text } from "@chakra-ui/react"
 import { Image } from '@chakra-ui/image'
+import { Tooltip } from "@chakra-ui/react"
 import { useHistory } from "react-router-dom"
 import { goToHomePage } from "../routes/coodinator"
 import imagem_pokedex_aberta from '../assets/Images/pokedex-aberta-kalos-removebg-preview.png'
@@ -13,14 +14,23 @@ const Pokedex = () => {
   return (
       <Box>
         <Header/>
-        <Image
-        cursor='pointer' 
-        h='70px' 
-        marginTop='-95px' 
-        marginLeft='25px' 
-        src={imagem_pokedex_aberta} 
-        onClick={() => goToHomePage(history)}
-        />
+
+        <Tooltip 
+        hasArrow label="Início" 
+        placement="right-start"
+        bgGradient="linear(#FF0000 60%, white 25%, white 0%)"
+        color='black'
+        >
+          <Image
+          cursor='pointer' 
+          h='70px' 
+          marginTop='-95px' 
+          marginLeft='25px' 
+          src={imagem_pokedex_aberta} 
+          onClick={() => goToHomePage(history)}
+          />
+        </Tooltip>
+
         <Box marginTop='25px'>
         <Text>Pokedex</Text>
         </Box>
